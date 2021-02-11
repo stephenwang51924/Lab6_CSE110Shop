57018ce363dc6f0f688313dff0fce91cd899dfed
Stephen <stephenwang51924@gmail.com>
2021-02-11 00:04:47 -0800

    finished

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,121 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(itemImage, itemTitle, itemPrice, itemId, itemStatus) {
+    super();
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    let style = document.createElement("style");
+    style.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }`;
+      
+      localStorage = window.localStorage;
+      let list = document.createElement("li");
+      list.setAttribute("class", "product");
+
+      let image = document.createElement("img");
+      image.setAttribute("src", itemImage);
+      image.setAttribute("alt", itemTitle);
+      image.setAttribute("width", "200");
+
+      let paragraph1 = document.createElement("p");
+      paragraph1.setAttribute("class", "title");
+      paragraph1.textContent = itemTitle;
+
+      let paragraph2 = document.createElement("p");
+      paragraph2.setAttribute("class", "price");
+      paragraph2.textContent = "$" + itemPrice;
+
+      let button = document.createElement("button");
+      button.setAttribute("onclick", "clickButton()");
+      button.textContent = itemStatus;
+      button.onclick = function()
+      {
+        let count = document.getElementById("cart-count");
+        if(button.textContent == "Add to Cart")
+        {
+          cartList.push(itemId);
+          button.textContent = "Remove from Cart";
+          alert("Added to Cart!");
+        }
+        else
+        {
+          cartList.splice(cartList.indexOf(itemId), 1);
+          button.textContent = "Add to Cart";
+          alert("Removed from Cart!");
+        }
+        count.textContent = cartList.length;
+        localStorage.setItem("cart", JSON.stringify(cartList));
+      }
+
+      list.appendChild(image);
+      list.appendChild(paragraph1);
+      list.appendChild(paragraph2);
+      list.appendChild(button);
+      shadowRoot.appendChild(style);
+      shadowRoot.appendChild(list);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,36 @@
 // Script.js
+let localStorage = window.localStorage;
+let cartList = [];
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener('DOMContentLoaded', async () => {
+  let response = await fetch("https://fakestoreapi.com/products");
+  if(response.ok)
+  {
+    let json = await response.json();
+    if(localStorage.getItem("array") == null)
+    {
+      localStorage.setItem("array", json);
+    }
+    let productList = document.getElementById("product-list");
+    if(localStorage.getItem("cart") != null)
+    {
+      cartList = JSON.parse(localStorage.getItem("cart"));
+      document.getElementById("cart-count").textContent = cartList.length;
+    }
+    for(let i = 0; i < json.length; i++)
+    {
+      let itemStatus;
+      if(cartList.includes(json[i].id))
+      {
+        itemStatus = "Remove from Cart";
+      }
+      else
+      {
+        itemStatus = "Add to Cart";
+      }
+      let product = new ProductItem(json[i].image, json[i].title, json[i].price, json[i].id, itemStatus);
+      productList.appendChild(product);
+    }
+  }
 });
+
